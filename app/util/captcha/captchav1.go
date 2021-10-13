20a79450236c3e4e2910be266b9bf2b140620c57
cilidm <cilidm>
2021-10-13 18:29:20 +0800

    增加数字运算验证码

--- app/util/captcha/captchav1.go
+++ app/util/captcha/captchav1.go
@@ -1,9 +1,9 @@
 package captcha
 
 import (
-	"fmt"
 	"github.com/mojocn/base64Captcha"
-	e2 "pear-admin-go/app/global/e"
+	"image/color"
+	e "pear-admin-go/app/global/e"
 )
 
 // 设置自带的store
@@ -11,30 +11,43 @@ var store = base64Captcha.DefaultMemStore
 
 //生成验证码
 func CaptMake() (id, b64s string, err error) {
-	var driver base64Captcha.Driver
-	var driverString base64Captcha.DriverString
-
-	// 配置验证码信息
-	captchaConfig := base64Captcha.DriverString{
-		Height:          e2.ImgHeight,
-		Width:           e2.ImgWidth,
-		NoiseCount:      0,     // 干扰字母
-		ShowLineOptions: 1 | 3, // 干扰线
-		Length:          e2.ImgKeyLength,
-		Source:          "qwertyuioplkjhgfdsazxcvbnm",
-	}
-
-	driverString = captchaConfig
-	driver = driverString.ConvertFonts()
+	driver := mathCaptcha()
 	captcha := base64Captcha.NewCaptcha(driver, store)
 	lid, lb64s, lerr := captcha.Generate()
 	return lid, lb64s, lerr
 }
 
+// 数字运算验证码
+func mathCaptcha() base64Captcha.Driver {
+	return base64Captcha.NewDriverMath(
+		e.ImgHeight,
+		e.ImgWidth,
+		0,
+		0,
+		&color.RGBA{0, 0, 0, 0},
+		[]string{"RitaSmith.ttf"},
+	)
+}
+
+// 字符验证码
+func stringCaptcha() base64Captcha.Driver {
+	var driver base64Captcha.Driver
+	var driverString base64Captcha.DriverString
+	captchaConfig := base64Captcha.DriverString{
+		Height:          e.ImgHeight,
+		Width:           e.ImgWidth,
+		NoiseCount:      0,     // 干扰字母
+		ShowLineOptions: 1 | 3, // 干扰线
+		Length:          e.ImgKeyLength,
+		Source:          "qwertyuioplkjhgfdsazxcvbnm",
+	}
+	driverString = captchaConfig
+	driver = driverString.ConvertFonts()
+	return driver
+}
+
 //验证captcha是否正确
 func CaptVerify(id string, capt string) bool {
-	fmt.Println("id:" + id)
-	fmt.Println("capt:" + capt)
 	if store.Verify(id, capt, false) {
 		return true
 	} else {
